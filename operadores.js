8e5e3a8bdb43dd3d2ffb55e5f1247da9fa593a57
Bruno Alencar <joaobrunoalencar@gmail.com>
2021-04-17 15:55:59 -0300

    Add operators and variables

---
+++ operadores.js
@@ -0,0 +1,24 @@
+
+
+// Operadores ( atalho para comentário no vscode = CTRL + ;)
+
+// Atribuição ( = )
+    // const idade = 2
+
+
+// Comparação
+    // == igual
+    // != diferente 
+    // >= maior que 
+    // <= menor que
+
+
+// console.log(2 == 2, 'igual')
+// console.log(2 != 2, 'diferente')
+// console.log(2 > 2, 'maior que')
+// console.log(2 < 2, 'menor que')
+// console.log(2 >= 2, 'maior ou igua')
+// console.log(2 <= 2, 'menor ou igual')
+// const a = 1;
+// const b = 2;
+// console.log((a + b) == 3 )
---
+++ variaveis.js
@@ -0,0 +1,66 @@
+// console.log('Hello world!')
+// CTRL + S salvar
+// <- comentários não são executados
+
+// string ex
+// var pessoa = "Bruno" 
+// console.log("Esse é meu nome " + pessoa)
+
+// const é variável utilazada constantes
+// Number ex
+// const numeroEscolhido = 25
+// console.log("Minha idade é " + numeroEscolhido)
+
+// Boleano ex 0,1
+// let variável fraca ( usada para ser substituida )
+// let verdadeiro = true;
+// let falso = false;
+
+// Objeto ex
+// let objetoCarro = {
+//   numeroDePortas: 4,
+//   cor: 'Azul'
+// }
+// console.log("Meu carro tem: " + objetoCarro.numeroDePortas + " portas") 
+// console.log("Meu carro tem: " + objetoCarro['numeroDePortas'] + " portas") 
+
+
+// Array           0,      1,        2
+// let pessoas = [ 'Bruno', 'Carlos', 'Ana' ] 
+// let idades = [ 25, 25, 53 ]
+// let listaDeCompras = ['tomate', 231, {name:'ana'}]
+// console.log(pessoas)
+// console.log(idades)
+// console.log(pessoas[2] + " tem " + idades[1])
+
+// typeof
+// const nome = 'Bruno'
+// console.log(typeof nome)
+
+// const idade = 25
+// console.log(typeof idade)
+
+// const carro = {
+//     cor: 'preto'
+// }
+// console.log(typeof carro)
+
+// const listaDeCompras = [
+//     'arroz',
+//     'feijão'
+// ]
+// console.log(typeof listaDeCompras)
+
+// const verdadeiro = false
+// console.log(typeof verdadeiro)
+
+/* Comentário com várias linhas
+  * string
+  * number
+  * object
+  * object
+  * boolean
+*/
+
+
+
